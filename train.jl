d44ba25e5f860a0bbdda111f0340061e5955423f
marc <monkeyusage@gmail.com>
2024-05-02 18:22:41 +0200

    only have on bot that trains on its own

--- train.jl
+++ train.jl
@@ -49,9 +49,9 @@ function pick_move(bot::Bot, board::Chess.Board)::Chess.Move
   # get the highest ranked legal prediction
   (policy, (board_value,)) = bot.neuralnet(to_array(board))
   # cancel impossible moves such as a1a1
-  predictions[[1:65:4096]...] .= 0
+  policy[[1:65:4096]...] .= 0
   # sort by highest probability
-  moves = sortperm(vec(predictions))
+  moves = sortperm(vec(policy))
   max_index = length(moves)
   move_index = 1 
   selected_move = Chess.Move(moves[move_index])
@@ -65,36 +65,22 @@ function pick_move(bot::Bot, board::Chess.Board)::Chess.Move
   selected_move
 end
 
-function play!(whites::Bot, blacks::Bot)
+function play!(bot::Bot)
   game = Chess.Game()
   while !Chess.isterminal(game)
     board = game.node.board
-    move = Chess.sidetomove(board) == Chess.WHITE ? pick_move(whites, board) : pick_move(blacks, board)
+    move = pick_move(bot, board)
     Chess.domove!(game, move)
   end
   if Chess.ischeckmate(game.node.board)
-    winner = Chess.sidetomove(game.node.board) == Chess.WHITE ? blacks : whites
-    winner.wins += 1
-  end
-end
-
-
-function round_robin!(bots::Vector{Bot})
-  for (idx, bot) in enumerate(bots)
-    for other_idx in 1:length(bots)
-      # dont play with yourself
-      if idx == other_idx
-        continue
-      end
-      play!(bot, bots[other_idx])
-    end
+    bot.wins += 1
   end
 end
 
 
 function train()
-  bots = [Bot() for _ in 1:10]
-  while sum(map((b) -> b.wins, bots)) == 0
-    round_robin!(bots)
+  bot = Bot()
+  while bot.wins == 0
+    play!(bot, bot)
   end
 end
