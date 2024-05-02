ebfe087aa41f4e373a3f4b74d38a4da952352fe5
marc <monkeyusage@gmail.com>
2024-05-02 18:15:39 +0200

    the best

--- train.jl
+++ train.jl
@@ -1,6 +1,15 @@
 import Chess
 import Flux
-using Debugger
+
+
+struct Cerberus{T}
+  paths::T
+end
+
+Cerberus(paths...) = Cerberus(paths)
+(model::Cerberus)(x::AbstractArray) = map(f -> f(x), model.paths)
+
+Flux.@layer Cerberus
 
 
 mutable struct Bot
@@ -11,13 +20,15 @@ end
 
 Bot() = Bot(
   Flux.Chain(
-    Flux.Conv((8, 8), 12 => 100, Flux.relu, bias=false, pad=Flux.SamePad()),
-    Flux.Conv((8, 8), 100 => 100, Flux.relu, bias=false, pad=Flux.SamePad()),
-    Flux.Conv((8, 8), 100 => 100, Flux.relu, bias=false, pad=Flux.SamePad()),
+    Flux.Conv((8, 8), 12 => 64, Flux.relu, bias=false, pad=Flux.SamePad()),
+    Flux.Conv((8, 8), 64 => 64, Flux.relu, bias=false, pad=Flux.SamePad()),
+    Flux.Conv((5, 5), 64 => 64, Flux.relu, bias=false, pad=Flux.SamePad()),
     Flux.flatten,
-    Flux.Dense(6400 => 64*63*4, Flux.relu),
     # output represents all possible moves in chess board for both white and black)
-    Flux.Dense(64*63*4 => 64*63*2, Flux.sigmoid),
+    Cerberus(
+      Flux.Dense(64*64 => 64*64, Flux.sigmoid),
+      Flux.Dense(64*64 => 1, Flux.tanh)
+    )
   ), 0
 )
 
@@ -36,20 +47,25 @@ end
 
 function pick_move(bot::Bot, board::Chess.Board)::Chess.Move
   # get the highest ranked legal prediction
-  predictions = bot.neuralnet(to_array(board))
+  (policy, (board_value,)) = bot.neuralnet(to_array(board))
+  # cancel impossible moves such as a1a1
+  predictions[[1:65:4096]...] .= 0
+  # sort by highest probability
   moves = sortperm(vec(predictions))
   max_index = length(moves)
   move_index = 1 
   selected_move = Chess.Move(moves[move_index])
-  while !(selected_move in Chess.moves(board))
+  # iterate over moves stop at first highest ranked legal move
+  legal_moves = Chess.moves(board)
+  while !(selected_move in legal_moves)
     move_index += 1
-    selected_move = Chess.Move(moves[move_index])
-    @assert move_index < max_index
+    @inbounds selected_move = Chess.Move(moves[move_index])
+    @assert move_index < max_index "legal_moves: $(legal_moves)"
   end
   selected_move
 end
 
-function play!(whites::Bot, blacks::Bot)::Nothing
+function play!(whites::Bot, blacks::Bot)
   game = Chess.Game()
   while !Chess.isterminal(game)
     board = game.node.board
@@ -57,15 +73,28 @@ function play!(whites::Bot, blacks::Bot)::Nothing
     Chess.domove!(game, move)
   end
   if Chess.ischeckmate(game.node.board)
-    winner = Chess.sidetomove(board) == Chess.WHITE ? blacks : whites
+    winner = Chess.sidetomove(game.node.board) == Chess.WHITE ? blacks : whites
     winner.wins += 1
   end
-  print(game.node.board)
 end
 
 
-function test()
-  bot = Bot()
-  board = Chess.@startboard
-  @time play!(bot, bot)
+function round_robin!(bots::Vector{Bot})
+  for (idx, bot) in enumerate(bots)
+    for other_idx in 1:length(bots)
+      # dont play with yourself
+      if idx == other_idx
+        continue
+      end
+      play!(bot, bots[other_idx])
+    end
+  end
+end
+
+
+function train()
+  bots = [Bot() for _ in 1:10]
+  while sum(map((b) -> b.wins, bots)) == 0
+    round_robin!(bots)
+  end
 end
